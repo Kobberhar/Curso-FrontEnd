8db51f5b2759b8e56c6513209d1cb7feb7e9a424
Kobberhar <claudio.navarro.riveros@gmail.com>
2021-03-02 20:15:30 -0300

    Agregando todos los archivos de Curso FrontEnd

---
+++ 6 JS Javascript/6.1 Declaracion de variables y tipos de datos/6.1.2 Tipos de datos/index.js
@@ -0,0 +1,10 @@
+console.log('Hola Mundo')
+
+// Tipos de datos en JS
+// String: Cadena de caracteres. 'a' 'hola' 'chanchito feliz'
+// String: Cadena de caracteres. "a" "Hola" "Chanchito feliz"
+// Boolean: true false.
+// Null: null o nulo.
+// Number: numero entero. 123123212 ["123"(String) != 123(Number)]
+// Undefined: Sin definir.
+// Object: objeto(cualquier combinacion de tipos de datos, funciones, etc...)
