50d4deebf8663bd10acc47dc5134325bb7d86781
Kobberhar <claudio.navarro.riveros@gmail.com>
2021-03-14 21:55:44 -0300

    Reordenamiento de las carpetas

---
+++ 4 JS Javascript/4.6 Mas funcionalidades de JS/4.6.3 Promesas: reject y catch/main.js
@@ -0,0 +1,19 @@
+//SPREAD OPERATOR
+
+// const a = {b:1} // Se crea variable 'a'
+// const b = a // Se crea variable 'b' = 'a'
+// b.c = 2 // Se agrega 'c' a 'b', lo que ademas modifica a 'a'
+
+// const c = {...a} // Spread Operator crea un objeto con todas las propiedades del objeto 'a' pero es independiente de 'a'
+// c.d = 12
+
+
+//PROMESAS
+
+// Promise.resolve(2) // Devuelve la API de promesa
+//     .then(valor => console.log(valor)) // Ejecuta una funcion a la cual se le entrega el valor de la promesa cuando sea resuelta
+
+Promise.reject(2) // Devuelve la API de promesa
+    .then(valor => valor + 1) // Resuelve una funcion y entrega un valor
+    .then(valor => console.log(valor)) // Entrega el valor final
+    .catch(e => console.error(e))
