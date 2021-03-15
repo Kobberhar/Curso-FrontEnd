43e015c5613ac400ac2d34fb3e5f99747c89f39f
Kobberhar <claudio.navarro.riveros@gmail.com>
2021-03-14 22:28:54 -0300

    Entrando al mundo NodeJS

---
+++ 5 NodeJS, Montando nuestra propia API rest Serverless con express JS/5.1 Introducción a NPM, express y API rest/main.js
@@ -0,0 +1,8 @@
+// npm init -y  // Inicia la instalacion en NPM y genera un archivo package.json
+
+// VERBOS HTTP              RECURSOS                    CODIGOS
+//GET   --> Listar          /Users      /Users/:id      200
+//POST  --> Crear           /Users                      201
+//PUT   --> Reemplazar      /Users/:id                  204
+//PATCH --> Actualizar      /Users/:id                  204
+//DELETE--> Eliminar        /Users/:id                  204
